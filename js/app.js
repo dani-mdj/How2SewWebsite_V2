6f7e2dad60f3888b3c2e0fedba71450ae2603461
Danielle <166341005+dani-mdj@users.noreply.github.com>
2025-05-10 16:05:27 -0400

    Updated class descriptions
    Added Json file with classes and made the classes linked. Then updated CSS for Classes and class items.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,63 @@
-const dteNow = new Date();
-const intYear = dteNow.getFullYear();
+(function (app) {
+  app.classItems = {};
+  app.selectedItem = {};
 
-copyright.innerHTML = "&copy " + intYear + " Copyright Danielle Jenson";
+  app.all = function () {
+    setCopyright();
+  };
+
+  app.showClass = function () {
+    loadClassData();
+    loadClassPage();
+    updateClassPage();
+  };
+
+  function setCopyright() {
+    const dteNow = new Date();
+    const intYear = dteNow.getFullYear();
+
+    const copyright = document.getElementById("copyright");
+
+    copyright.innerHTML = "&copy " + intYear + " Copyright Danielle Jenson";
+  }
+
+  async function loadClassData() {
+    const rawData = await fetch("JSON/class.json");
+    const data = await rawData.json();
+    app.classItem = data;
+    sessionStorage.setItem("site-data", JSON.stringify(data));
+    app.classItems = JSON.parse(cacheData);
+    const cacheData = sessionStorage.getItem("site-data");
+  }
+
+  function loadClassPage() {
+    const params = new URLSearchParams(window.location.search);
+    let classCode = params.get("class");
+    let classIndex = null;
+    if (classCode in app.classItems[0].index) {
+      classIndex = app.classItems[0].index[classCode];
+    } else {
+      classIndex = 1;
+    }
+    app.selectedItem = app.classItems[classIndex];
+  }
+
+  function updateClassPage() {
+    let elementKeys = Object.keys(app.selectedItem);
+    elementKeys = elementKeys.splice(1, elementKeys.length - 1);
+    for (const element of elementKeys) {
+      if (element == "req-tools" || element == "req-materials") {
+        const section = document.getElementById(element);
+        let htmlText = "<ul>";
+        for (const item of app.selectedItem[element]) {
+          htmlText += "<li>" + item + "</li>";
+          console.log(item);
+        }
+        htmlText += "</ul>";
+        section.innerHTML = htmlText;
+      } else {
+        document.getElementById(element).innerText = app.selectedItem[element];
+      }
+    }
+  }
+})((window.app = window.app || {}));
